0871009d370e59d7ad12e84b6bc24e3d76d9e051
Luis Felipe <luis.felipe32971@gmail.com>
2022-05-31 20:10:38 -0300

    Adding setup and teardown class.

--- test_teardown1.py
+++ test_teardown1.py
@@ -1,34 +1,34 @@
 
-def setup_module():
-    print("Setup Module!")
 
-
-def teardown_module():
-    print("Teardown Module")
-
-def setup_function(function):
-    if function == test1:
-        print("\nSetting up test1")
-    elif function == test2:
-        print("\nSetting up test2")
-    else:
-        print("\nSetting up unkknown test")
-
-
-def teardown_function(function):
-    if function == test1:
-        print("\nTearing down test1")
-    elif function == test2:
-        print("\nTearing down test2")
-    else:
-        print("\nTearing down unkknown test")
-
-
-def test1():
-    print("Executing test 1!")
-    assert True
-
-
-def test2():
-    print("Executing test 2!")
-    assert True
+class TestClass:
+    @classmethod
+    def setup_class(cls):
+        print("\nSetup TestClass!")
+    
+    @classmethod
+    def teardown_class(cls):
+        print("Teardown TestClass")
+    
+    def setup_method(self, method):
+        if method == self.test1:
+            print("\nSetting up test1")
+        elif method == self.test2:
+            print("\nSetting up test2")
+        else:
+            print("\nSetting up unkknown test")
+    
+    def teardown_method(self, method):
+        if method == self.test1:
+            print("\nTearing down test1")
+        elif method == self.test2:
+            print("\nTearing down test2")
+        else:
+            print("\nTearing down unkknown test")
+    
+    def test1(self):
+        print("Executing test 1!")
+        assert True
+    
+    def test2(self):
+        print("Executing test 2!")
+        assert True
