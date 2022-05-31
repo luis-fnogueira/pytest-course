fa387f9d66d424c29b074fc9115526cc537f8a35
Luis Felipe <luis.felipe32971@gmail.com>
2022-05-31 20:35:00 -0300

    Using request.addfinalizer.

--- test_fixtures.py
+++ test_fixtures.py
@@ -2,16 +2,31 @@ import pytest
 
 
 @pytest.fixture()  # Also can use autouse = True, so it's not necessary to pass it as param
-def setup():
-    print("\nSetup!")
+def setup1():
+    print("\nSetup 1!")
+    yield
+    print("\nTeardown 1!")
 
 
-def test1(setup):
+@pytest.fixture()
+def setup2(request):
+    print("\nSetup 2")
+
+    def teardown_a():
+        print("\nTeardown A!")
+
+    def teardown_b():
+        print("\nTeardown B!")
+
+    request.addfinalizer(teardown_a)
+    request.addfinalizer(teardown_b)
+
+
+def test1(setup1):
     print("Executing test 1")
     assert True
 
 
-@pytest.mark.usefixtures("setup")
-def test2():
+def test2(setup2):
     print("Executing test 2")
     assert True
