e6b4a89e7242f3135453fb974b1c4dc6355504ef
Luis Felipe <luis.felipe32971@gmail.com>
2022-05-31 19:55:36 -0300

    First version of teardown and setup.

---
+++ test_teardown1.py
@@ -0,0 +1,27 @@
+
+def setup_function(function):
+    if function == test1:
+        print("\nSetting up test1")
+    elif function == test2:
+        print("\nSetting up test2")
+    else:
+        print("\nSetting up unkknown test")
+
+
+def teardown_function(function):
+    if function == test1:
+        print("\nTearing down test1")
+    elif function == test2:
+        print("\nTearing down test2")
+    else:
+        print("\nTearing down unkknown test")
+
+
+def test1():
+    print("Executing test 1!")
+    assert True
+
+
+def test2():
+    print("Executing test 2!")
+    assert True
